ebe29fa0634abb8d5b0b2626189222ac55552a40
yts28075086 <59994731+yts28075086@users.noreply.github.com>
2021-09-16 14:44:16 +0800

    test

---
+++ app/build/generated/source/buildConfig/debug/edu/hkbu/comp/comp4097/infodat/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.hkbu.comp.comp4097.infodat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.hkbu.comp.comp4097.infodat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
